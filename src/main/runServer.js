9a2224b6ec13218d068d19be1c53cba7814ad406
Guo Liu <liuguomailbox@gmail.com>
2019-09-07 19:52:33 -0700

    add remote schema

--- src/main/runServer.js
+++ src/main/runServer.js
@@ -1,7 +1,31 @@
 import { ipcMain } from 'electron'
 // NOTE: The library ships with a fork of apollo-link-schema that also supports subscriptions.
 import { createSchemaLink, createIpcExecutor } from 'graphql-transport-electron'
-import schema from './my-graphql-schema'
+
+import {
+  makeRemoteExecutableSchema,
+  introspectSchema,
+  mergeSchemas
+} from 'graphql-tools'
+import { HttpLink } from 'apollo-link-http'
+import fetch from 'node-fetch'
+
+// import schema from './my-graphql-schema'
+
+const remoteUrl = 'https://server-test.matters.news/graphql'
+const remoteLink = new HttpLink({
+  uri: remoteUrl,
+  fetch
+})
+// Introspect schema
+const remoteSchema = await introspectSchema(remoteLink)
+// Make remote executable schema
+const remoteExecutableSchema = makeRemoteExecutableSchema({
+  schema: remoteSchema,
+  remoteHttpLink
+})
+
+const schema = {}
 
 // NOTE: This also works with any other apollo link (e.g. HttpLink from apollo-link).
 const link = createSchemaLink({ schema })
